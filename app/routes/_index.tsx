cb17d959ddfd0b6361ad2a18b96c6f4aab1c2c33
Yukiya Nakagawa <yukiya@monicle.co.jp>
2023-07-18 11:10:07 +0900

    treat index

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,8 +1,9 @@
 import type { V2_MetaFunction } from "@remix-run/cloudflare";
+import { Link } from "@remix-run/react";
 
 export const meta: V2_MetaFunction = () => {
   return [
-    { title: "New Remix App" },
+    { title: "Remix Networking Sample" },
     { name: "description", content: "Welcome to Remix!" },
   ];
 };
@@ -10,30 +11,13 @@ export const meta: V2_MetaFunction = () => {
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
+      <h1>Welcome to Remix Networking Sample</h1>
       <ul>
         <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
+          <Link to="/1998">1998</Link>（with native <code>&lt;form&gt;</code>）
         </li>
         <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
+          <Link to="/2023">2023</Link>（with remix <code>&lt;Form&gt;</code>）
         </li>
       </ul>
     </div>
